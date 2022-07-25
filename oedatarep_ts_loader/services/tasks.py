d9f7737e444d4893913314f85311cf8de7bb623d
fpistagna <fabrizio.pistagna@ingv.it>
2022-07-25 14:54:27 +0200

    Add interface logic between OEDatarep-TSDSystem.

--- oedatarep_ts_loader/services/tasks.py
+++ oedatarep_ts_loader/services/tasks.py
@@ -7,20 +7,69 @@
 # more details.
 
 from celery import shared_task
+from flask import current_app
 from .search import TSRecordsSearch
+from .ts_resource import TSResource
+
 @shared_task
 def register_ts():
 	records = TSRecordsSearch().execute()
 	ids = []
+	conf = current_app.config
+
 	for record in records:
 		ids.append(record.id)
-		# TODO: BOotstrap TS loader workflow
-		# TSResource().bootstrap_instance()
+
+		ts_object = TSResource(conf.get("OEDATAREP_API_RECORDS_URL"))
+		ts_object.bootstrap_instance(record.id)
+
+		record_data = ts_object.get_record_data()
+		ts_object.set_instance_metadata(record_data)
+		ts_object.set_instance_links(record_data)
+
+		try:
+			current_app.logger.debug("Instance _file_link: %s", ts_object._file_link)
+
+			if ts_object._file_link != "":
+				if ts_object.get_record_files():
+					if ts_object.filter_ts_files():
+						tsd_token = ts_object.get_tsd_auth_token()
+
+						for entry in ts_object.ts_files_to_upload:
+							ts_guid = ""
+							header_data = ts_object.get_ts_header_file(entry[1]["content_link"], tsd_token)
+
+							if bool(header_data):
+								header_data["name"] = header_data["name"]+"_"+str(record.id).replace("-","")
+								ts_guid = ts_object.post_ts_header_on_tsd(header_data, tsd_token)
+
+								current_app.logger.debug("Record (id:%s) - TS_Guid: %s", record.id, ts_guid)
+
+								# if bool(ts_guid):
+								# 	ts_object.put_ts_guid_on_record_metadata(ts_guid)
+							else:
+								current_app.logger.info("Record (id:%s) - has no TS_Header", record.id)
+
+							ts_data = ts_object.get_ts_data_file(entry[0]["content_link"], ts_guid, tsd_token)
+							# ts_data = ts_object.get_ts_data_file(entry[0]["content_link"], tsd_token)
+
+							if bool(ts_data):
+								current_app.logger.debug("Record (id:%s) - TS_Data: %s", record.id, bool(ts_data))
+								ts_object.post_ts_data_on_tsd(ts_data, tsd_token)
+
+								if bool(ts_guid):
+									ts_object.put_ts_guid_on_record_metadata(ts_guid)
+							else:
+								current_app.logger.info("Record (id:%s) - has no TS_data", record.id)
+					else:
+						current_app.logger.info("Record (id:%s) - does not have a valid TS pair files(csv,json)", record.id)
+				else:
+					current_app.logger.info("Record (id:%s) - has no files", record.id)
+			else:
+				current_app.logger.info("Record (id:%s) - has no links resource to files", record.id)
+
+		except Exception as e:
+			current_app.logger.exception(e)
+			raise e
+
 	return " ".join(ids)
-
-class TSResource():
-	def __init__(self, url=None, token=None):
-		pass
-
-	def bootstrap_instance(self):
-		pass
