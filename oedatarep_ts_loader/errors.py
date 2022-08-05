5bd106b3f52d27a7f0dd2865d33649ae01eb819c
fpistagna <fabrizio.pistagna@ingv.it>
2022-08-05 15:37:15 +0200

    Add Exception handlers. Code refactor.

--- oedatarep_ts_loader/errors.py
+++ oedatarep_ts_loader/errors.py
@@ -8,3 +8,33 @@ class TSLoaderException(Exception):
     def __init__(self, message="Default message."):
         self.message = message
         super().__init__(self.message)
+
+class RecordMissingFiles(TSLoaderException):
+    """
+        Exception raised due to missing files in record definition
+        (one between csv and json has not been loaded)
+    """
+    def __init__(self, message="No TS files found."):
+        self.message = (f"No TS files (csv,json) "
+                        f"in record definition.")
+        super().__init__(self.message)
+
+class HeaderFileMissing(TSLoaderException):
+    """
+        Exception raised when Header file is missing in record definition
+        (only csv has been uploaded)
+    """
+    def __init__(self):
+        self.message = (f"No .json file Header "
+                        f"found in record definition for csv.")
+        super().__init__(self.message)
+
+class TSDataFileMissing(TSLoaderException):
+    """
+        Exception raised when TS DAta file is missing in record definition
+        (only json has been uploaded)
+    """
+    def __init__(self):
+        self.message = (f"No .csv file Data "
+                        f"found in record definition for header.")
+        super().__init__(self.message)
--- oedatarep_ts_loader/services/tasks.py
+++ oedatarep_ts_loader/services/tasks.py
@@ -7,18 +7,22 @@
 # more details.
 
 import logging
+
 from celery import shared_task
 from flask import current_app
 
-from ..errors import TSLoaderException
+from ..errors import (
+    TSLoaderException, 
+    RecordMissingFiles, 
+    HeaderFileMissing, 
+    TSDataFileMissing)
 from .components.datarep import OEDataRep
 from .components.tsd_system import TSDSystem
 from .search import TSRecordsSearch
 
 logger = logging.getLogger("oedatarep-ts-loader")
 
-
-@shared_task
+@shared_task()
 def register_ts():
     """ Task that starts time series pubblication on TSDSystem. """
     records = TSRecordsSearch().execute()
@@ -39,7 +43,7 @@ def execute_register_ts(recid):
     oedatarep = OEDataRep()
     tsd_system = TSDSystem()
     try:
-        current_record = oedatarep.get_record_data("recid")
+        current_record = oedatarep.get_record_data(recid)
         record_files = oedatarep.get_record_files(
                     current_record["links"]["files"]
         )
@@ -69,17 +73,20 @@ def execute_register_ts(recid):
             print(ts_resource)
         current_record["metadata"]["ts_resources"] = ts_resources
         oedatarep.update_record_metadata(recid, current_record)
-    except Exception as ex:
-        ts_resources.append({"ERRORE"})
-    return (recid, ts_resources)
+        
+    except (RecordMissingFiles, TSDataFileMissing, HeaderFileMissing) as ts_ex:
+        logger.warning("Record id: %s - %s", recid, ts_ex.message)
+    except (TSLoaderException, Exception) as ex:
+        raise ex
 
+    return (recid, ts_resources)
 
 def __parse_record_files(record_files):
     res = []
 
     try:
         for r in record_files["entries"]:
-            current_app.logger.debug("Entry: %s", r)
+            # current_app.logger.debug("Entry: %s", r)
             if r["status"] == "completed":
                 res.append(dict({
                     "key": r["key"],
@@ -92,49 +99,45 @@ def __parse_record_files(record_files):
     finally:
         if len(res) > 0:
             current_app.logger.debug("GOT record_files: %s", res)
-
+        else:
+            raise RecordMissingFiles()
     return res
 
+def __check_ts_files_pair(files):
+    data_entries = {}
+    header_entries = {}
+
+    try:
+        for f in files:
+            if f['mimetype'] in 'text/csv':
+                data_entries[f['key'].replace(".csv", "")] = f
+            elif f['mimetype'] in 'application/json':
+                header_entries[f['key'].replace("_header.json", "")] = f
+
+        if len(data_entries.keys()) < len(header_entries.keys()):
+            raise TSDataFileMissing()
+        elif len(data_entries.keys()) > len(header_entries.keys()):
+            raise HeaderFileMissing()
+
+    except ValueError as err:
+        raise (err)
+    
+    else:
+        return (data_entries, header_entries)
 
 def __filter_ts_files(record_files):
     """ Returns a list with each element a .csv & .json pair. """
     res = {}
     files = __parse_record_files(record_files)
-    try:
-        data_entries = [d for d in files if d['mimetype'] in 'text/csv']
-        header_entries = [h for h in files if h['mimetype'] in
-                          'application/json']
+    data_entries = {}
+    header_entries = {}
 
-        # current_app.logger.info("TS_Data_Array: %s", data_entries)
-        # current_app.logger.info("TS_Header_Array: %s", header_entries)
-    except ValueError as err:
-        raise (err)
+    (data_entries, header_entries) = __check_ts_files_pair(files)
 
-    for d_entry in data_entries:
-        # current_app.logger.info("TS_Entry: %s", d_entry)
-        for h_entry in header_entries:
-            # current_app.logger.info("Header_Entry: %s", h_entry)
-            try:
-                if h_entry['key'] in d_entry["key"].replace(".csv",
-                                                            "_header.json"):
-                    name = d_entry["key"].replace(".csv", "")
-                    res[name] = dict({
-                        "csv": d_entry,
-                        "header": h_entry
-                    })
-                    # res.append([
-                    #     d_entry,
-                    #     [h_entry for h_entry in header_entries
-                    #         if h_entry['key'] in d_entry["key"].replace(
-                    #             ".csv", "_header.json")].pop()]
-                    # )
-            except ValueError as err:
-                raise (err)
-
-    # if len(data_entries) > 0 and len(header_entries) > 0:
-    #     current_app.logger.info("Instance data(s) to Upload: %s", res)
-    # else:
-    #     current_app.logger.debug("Instance data: %s \n Intance header: %s",
-    #                              data_entries, header_entries)
+    for dk in data_entries.keys():
+        res[dk] = dict({
+            "csv": data_entries[dk],
+            "header": header_entries[dk]
+        })
 
     return res
