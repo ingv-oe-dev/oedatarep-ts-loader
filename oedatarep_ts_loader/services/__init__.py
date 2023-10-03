fa79a33444cb35cedb2269e907fe113fb53f3c82
mtorrisi <mario.torrisi@ingv.it>
2023-10-03 11:02:33 +0200

    Init custom fields implementation

--- oedatarep_ts_loader/services/__init__.py
+++ oedatarep_ts_loader/services/__init__.py
@@ -1,7 +1,8 @@
 from .components import OEDataRep
 from .components import TSDSystem
-
+from .custom_fields.timeseries import TimeseriesCF
 __all__ = (
     "TSDSystem",
     "OEDataRep",
+    "TimeseriesCF",
 )
